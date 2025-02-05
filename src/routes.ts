2ff760dce14b80e1dae592d66bb683562316c3e7
Nicola31082005 <nikule512@gmail.com>
2025-02-05 14:56:44 +0200

    add 404 page

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,13 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
 import { homeController } from "./controllers/homeController.js";
 
 const routes = Router();
 
 routes.use(homeController)
 
-
+routes.all('*', (req: Request, res: Response) => {
+    res.render('404')
+})
 
 
 export default routes
