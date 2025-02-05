238aa43a35a7176dd30eb05f1259ff31a974e53f
Nicola31082005 <nikule512@gmail.com>
2025-02-05 19:42:45 +0200

    add jwt token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { Request, Response } from "express";
 import { register } from "../services/authService.js";
 import UserData from "../types/user-type.js";
+import { console } from "inspector";
 
 
 const authController = Router();
@@ -20,6 +21,17 @@ authController.post('/register', async (req: Request, res: Response) => {
     try {
         await register(data)
         res.redirect('/auth/login')
+    } catch (error) {
+        console.error(error)
+        res.end()
+    }
+})
+
+authController.post('/login', async (req: Request, res: Response) => {
+    const {email, password}: UserData = req.body;
+    
+    try {
+        // await login
     } catch (error) {
         console.error(error)
     }
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,15 +1,40 @@
 import User from "../models/user-model.js"
 import UserData from "../types/user-type.js"
+import bcrypt from 'bcrypt';
+import jwt, { Secret } from 'jsonwebtoken';
 
+
+const JWT_SECRET: Secret = process.env.JWT_SECRET!;
 
 export async function register(data: UserData){
-        if (!data) return
+    if (!data) return
 
-        const counts = await User.countDocuments({ email: data.email })        
-        if (counts > 0) {
-            throw new Error('Email is already taken.')
-        }
+    const counts = await User.countDocuments({ email: data.email })        
+    if (counts > 0) {
+        throw new Error('Email is already taken.')
+    }
 
-        return await User.create(data)    
+    return await User.create(data)    
+} 
+
+export async function login(email: string, password: string){
+    if (!email || !password) return
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+        throw new Error('Email or Password don\'t match!');
+    }
+
+    const isValid = bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error('Email or Password don\'t match!')
+    }
+
+    const payload = { id: user._id, email: user.email }
+    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '2h' });
+
+    return token;    
 } 
 
