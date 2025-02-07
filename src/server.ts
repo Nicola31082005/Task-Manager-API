4cf2ef12e7aef15d492bc9ded3c9eb57186d223b
Nicola31082005 <nikule512@gmail.com>
2025-02-07 02:23:03 +0200

    add moment date parser

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,43 @@
-import express, { Application } from 'express';
-import handlebars from 'express-handlebars';
-import routes from './routes.js';
-import path from 'path';
-import 'dotenv/config';
-import helmet from 'helmet';
-import DbConnect from './config/db.js';
-import cookieParser from 'cookie-parser';
+import express, { Application } from "express";
+import handlebars from "express-handlebars";
+import routes from "./routes.js";
+import path from "path";
+import "dotenv/config";
+import helmet from "helmet";
+import DbConnect from "./config/db.js";
+import cookieParser from "cookie-parser";
+import { registerhelper } from "./helpers/registerHelper.js";
 
 const app: Application = express();
 
-
 // Secure vulnurability
-app.use(helmet())
+app.use(helmet());
 
 // Body parser middleware
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 // Connect with DB
-DbConnect()
+DbConnect();
 
 // Handlebars config
-app.engine('hbs', handlebars.engine({ extname: 'hbs', runtimeOptions: { allowProtoPropertiesByDefault: true }}));
-app.set('view engine', 'hbs');
-app.set('views', path.join(process.cwd(), 'src', 'views'))
-
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+    runtimeOptions: { allowProtoPropertiesByDefault: true },
+    helpers: {
+      formatDate: registerhelper,
+    },
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(process.cwd(), "src", "views"));
 
 // Serve static files
-app.use(express.static(path.join(process.cwd(), "public")))
-app.use(routes)
+app.use(express.static(path.join(process.cwd(), "public")));
+app.use(routes);
 
-const PORT = process.env.PORT || 3000
-app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
