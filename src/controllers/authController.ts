b405f2afa358ca074f541bff29ee6c7f61714bee
Nicola31082005 <nikule512@gmail.com>
2025-02-08 22:21:20 +0200

    Add jwt after register

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -3,46 +3,46 @@ import { Request, Response } from "express";
 import { login, register } from "../services/authService.js";
 import UserData from "../types/user-type.js";
 
-
-
 const authController = Router();
 
-authController.get('/login', (req: Request, res: Response) => {
-    res.render('auth/login', { title: 'Login' })
-})
+authController.get("/login", (req: Request, res: Response) => {
+  res.render("auth/login", { title: "Login" });
+});
 
-authController.get('/register', (req: Request, res: Response) => {
-    res.render('auth/register', { title: 'Register' })
-})
+authController.get("/register", (req: Request, res: Response) => {
+  res.render("auth/register", { title: "Register" });
+});
 
-authController.post('/register', async (req: Request, res: Response) => {
-    const data: UserData = req.body;
-    
-    try {
-        await register(data)
-        res.redirect('/auth/register')
-    } catch (error) {
-        res.status(400).render('auth/register', { title: 'Register', error: error })      
-    }
-})
+authController.post("/register", async (req: Request, res: Response) => {
+  const data: UserData = req.body;
 
-authController.post('/login', async (req: Request, res: Response) => {
-    const {email, password}: UserData = req.body;
-    
-    try {
-        const token = await login(email, password)
-        res.cookie('auth', token, { httpOnly: true })
-        res.redirect('/')
-    } catch (error) {
-        res.status(400).render('auth/login', { title: 'Login', error: error })      
-    }
+  try {
+    const token = await register(data);
+    res.cookie("auth", token, { httpOnly: true });
+    res.redirect("/");
+  } catch (error) {
+    res
+      .status(400)
+      .render("auth/register", { title: "Register", error: error });
+  }
+});
 
-})
+authController.post("/login", async (req: Request, res: Response) => {
+  const { email, password }: UserData = req.body;
 
-authController.get('/logout', async (req: Request, res: Response) => {
-    res.clearCookie('auth');
-    
-    res.redirect('/')
-})
+  try {
+    const token = await login(email, password);
+    res.cookie("auth", token, { httpOnly: true });
+    res.redirect("/");
+  } catch (error) {
+    res.status(400).render("auth/login", { title: "Login", error: error });
+  }
+});
 
-export default authController
+authController.get("/logout", async (req: Request, res: Response) => {
+  res.clearCookie("auth");
+
+  res.redirect("/");
+});
+
+export default authController;
--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,41 +1,45 @@
-import User from "../models/user-model.js"
-import UserData from "../types/user-type.js"
-import bcrypt from 'bcrypt';
-import jwt, { Secret } from 'jsonwebtoken';
-import 'dotenv/config'
+import User from "../models/user-model.js";
+import UserData from "../types/user-type.js";
+import bcrypt from "bcrypt";
+import jwt, { Secret } from "jsonwebtoken";
+import "dotenv/config";
+import { Types } from "mongoose";
 
 const JWT_SECRET: Secret = process.env.JWT_SECRET!;
 
-export async function register(data: UserData){    
-    if (!data) return
+export async function register(data: UserData) {
+  if (!data) return;
 
-    const counts = await User.countDocuments({ email: data.email })        
-    if (counts > 0) {
-        throw new Error('Email is already taken.')
-    }
+  const counts = await User.countDocuments({ email: data.email });
+  if (counts > 0) {
+    throw new Error("Email is already taken.");
+  }
 
-    return await User.create(data)    
-} 
+  const user = await User.create(data);
+  return tokenGenerator(user._id, user.email);
+}
 
-export async function login(email: string, password: string){
-    if (!email || !password) return
+export async function login(email: string, password: string) {
+  if (!email || !password) return;
 
-    const user = await User.findOne({ email });
+  const user = await User.findOne({ email });
 
-    if (!user) {
-        throw new Error('Email or Password don\'t match!');
-    }
+  if (!user) {
+    throw new Error("Email or Password don't match!");
+  }
 
-    const isValid = await bcrypt.compare(password, user.password);
-    
-    if (!isValid) {
-        throw new Error('Email or Password don\'t match!')
-    }
-    
-    const payload = { id: user._id, email: user.email }
-    const token = jwt.sign(payload, JWT_SECRET, { expiresIn: '2h' });
+  const isValid = await bcrypt.compare(password, user.password);
 
-    return token;    
-} 
+  if (!isValid) {
+    throw new Error("Email or Password don't match!");
+  }
 
+  return tokenGenerator(user._id, user.email);
+}
 
+export function tokenGenerator(id: Types.ObjectId, email: string) {
+  const payload = { id, email };
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "2h" });
+
+  return token;
+}
